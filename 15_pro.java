5379ed05f67337fbafc90cb6240909b279236c58
Raghvendra Singh <32678356+Raghvendrars@users.noreply.github.com>
2021-03-04 09:05:02 +0530

    Create 15_pro.java

---
+++ 15_pro.java
@@ -0,0 +1,11 @@
+class raghav{
+
+    public static void main(String[] args) {
+        System.out.println("This is main class");
+        
+    }
+
+    static{
+        System.out.println("This is static class");
+    }
+}
