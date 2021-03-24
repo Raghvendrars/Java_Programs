fb81e886afa5f5d5c334291b41f8378411be2017
Raghvendra Singh <32678356+Raghvendrars@users.noreply.github.com>
2021-03-24 17:34:51 +0530

    Create result.java

---
+++ 25/exam/result.java
@@ -0,0 +1,12 @@
+import student.*;
+import exam.*;
+class prog25
+{
+	public static void main(String args[])
+	{
+		student obj1=new student();
+		result obj2=new result();
+		obj1.display();
+		obj2.display();
+	}
+}
