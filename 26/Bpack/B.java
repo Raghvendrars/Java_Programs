aa0960b38cab603eb5ab5f1a8972a800fb83cbbc
Raghvendra Singh <32678356+Raghvendrars@users.noreply.github.com>
2021-03-24 17:38:54 +0530

    Create B.java

---
+++ 26/Bpack/B.java
@@ -0,0 +1,14 @@
+package BPack;
+import APack.*;
+public class B extends A
+{
+	public void display()
+	{
+		System.out.println("B----------------------------B");
+		System.out.println("protected x : "+x);
+		//System.out.println("private y : "+y);//ERROR : can't inherited private variable
+		System.out.println("private y : "+getY());
+		System.out.println("public z : "+z);
+		System.out.println("B----------------------------B\n");
+	}
+}
