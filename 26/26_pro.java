cae6d1b06645bb759ef40a5f106f34110ae2c21c
Raghvendra Singh <32678356+Raghvendrars@users.noreply.github.com>
2021-03-24 17:37:25 +0530

    Create 26_pro.java

---
+++ 26/26_pro.java
@@ -0,0 +1,14 @@
+import BPack.*;
+import CPack.*;
+
+public class ProtectedDemo
+{
+	public static void main(String args[])
+	{
+
+		B objB = new B();
+		objB.display();
+		C objC = new C();
+		objC.display();
+	}
+}
